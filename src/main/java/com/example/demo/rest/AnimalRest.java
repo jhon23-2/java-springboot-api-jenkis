fae54ff4b58a3f679c729c444bcfd981cd5d3ed2
Jhonattan Thomas Almanza Campo <jhonattan.almanza@globant.com>
2026-08-31 09:54:29 -0500

    AnimalRest and AnimalService has been modified

--- src/main/java/com/example/demo/rest/AnimalRest.java
+++ src/main/java/com/example/demo/rest/AnimalRest.java
@@ -1,4 +1,28 @@
 package com.example.demo.rest;
 
+import com.example.demo.entity.Animal;
+import com.example.demo.service.AnimalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/animals")
+@RequiredArgsConstructor
 public class AnimalRest {
+    private final AnimalService animalService;
+
+    @PostMapping
+    public ResponseEntity<Animal> save(@RequestBody Animal animal) {
+        Animal saved = animalService.save(animal);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Animal>> findAll(){
+        return ResponseEntity.ok(animalService.findAll());
+    }
 }
--- src/main/java/com/example/demo/service/AnimalService.java
+++ src/main/java/com/example/demo/service/AnimalService.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Slf4j
 public class AnimalService {
 
-    private AnimalInterface animalInterface;
+    private final AnimalInterface animalInterface;
 
     public Animal save(Animal animal) {
         Animal animalSaved = animalInterface.save(animal);
